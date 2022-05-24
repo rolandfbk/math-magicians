410025d5d31148219323186fe5398961651f6c0f
Roland <rolandfbk@gmail.com>
2022-05-24 19:44:53 +0200

    code reviewed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,7 @@
-// import React, { Component } from 'react';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 class Calculator extends React.PureComponent {
-  /* constructor(props) {
-    super(props);
-  } */
-
   render() {
     return (
       <CalcComponent
